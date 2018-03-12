a241e7cf6b7561e099eac75c3475b41cce5bba32
D7703_07 <d7703_07@d7703-07ui-imac.local>
2018-03-12 15:54:13 +0900

    1234

---
+++ Button Fun 02/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Button Fun 02
+//
+//  Created by D7703_07 on 2018. 3. 12..
+//  Copyright © 2018년 D7703_07. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.cyan
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
